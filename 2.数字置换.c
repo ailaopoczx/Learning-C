ca5e96c3ad90b501d9d6961ac0ba3603b878b4d9
ailaopoczx <96461679+ailaopoczx@users.noreply.github.com>
2025-08-16 12:49:37 +0800

    Add files via upload

---
+++ 2.数字置换.c
@@ -0,0 +1,22 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <string.h>
+//#include <stdio.h>
+//
+//void Swap(int* px, int* py)
+//{
+//	int z = *px;
+//	*px = *py;
+//	*py = z;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("÷√ªª«∞ a = %d£¨ b = %d", a, b);
+//	Swap(&a, &b);
+//	printf("÷√ªª∫Û a = %d£¨ b = %d", a, b);
+//
+//
+//	return 0;
+//}
